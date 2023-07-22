cd490461b6154dcc86e6cc91787f8e1065fb7a13
Agung Gunawan <agg.nst772@gmail.com>
2023-07-22 10:44:55 +0700

    up

--- Assets/Script/GameplayManager.cs
+++ Assets/Script/GameplayManager.cs
@@ -9,9 +9,29 @@ public class GameplayManager : MonoBehaviour
     public static GameplayManager instance;
 
     public int stage;
+    public int demegeEnemy = 20;
 
     private void Awake()
     {
         instance = this;
+    }
+
+    private void Start()
+    {
+        stage = 1;
+    }
+
+    public void PlayerDeath()
+    {
+
+    }
+
+    public void UpgDemegePlayer()
+    {
+
+    }
+    public void UpgDemegeEnemy()
+    {
+
     }
 }
